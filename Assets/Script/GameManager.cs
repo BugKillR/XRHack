e03a24125dd806e9000ada1586673e61d06d7e37
Mert Ates Topuz <m.atestopuz@gmail.com>
2024-09-08 11:03:08 +0300

    Commit 28

--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -5,11 +5,22 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     public float score;
+    public GameObject[] enemys;
 
     public int health = 100;
 
     void Update()
     {
         Debug.Log($"Score: {score}");    
+
+        enemys = GameObject.FindGameObjectsWithTag("obj3");
+
+        if(health <=0 )
+        {
+            for (int i = 0; i < enemys.Length; i++)
+            {
+                Destroy(enemys[i].gameObject);
+            }
+        }
     }
 }
